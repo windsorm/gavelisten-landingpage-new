f11cb5ea4ba7a0034cc12263c4d0610d30e00d0d
Martin Windsor <martinwindsor1@gmail.com>
2022-01-11 11:33:01 +0100

    Landing Page structure

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            maingreen: '#42CB5D',
+            ppgray: '#272727'
+          },
+          fontFamily: {
+            'graphie': ['Graphie', 'sans-serif']
+          }
+        }
+      }
+    }
